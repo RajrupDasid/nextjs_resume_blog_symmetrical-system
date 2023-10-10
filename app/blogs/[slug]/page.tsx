013e17a7295584ac74be86bb6c7d3899a6816b21
Rajrup Das <rajrupofficialid@protonmail.com>
2023-10-10 03:49:05 +0000

    Bugsqashing and feature developments (#1)
    * Applied logic for slug
    
    Logic hasbeen applied for perticular post slug based link to went to the blog detail view
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * Query bug fixed
    
    sample query bug fixed
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * Slug based single page fetching
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * Bug fixed
    
    Blog detail page bug hasbeen fixed and applied some patches to coverup the issues as of now
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * deleted
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * Deleted
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * Added more information inside blog page
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * Auto added
    
    There is some code which is automatically added the proper settings in typescript
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * Added fix for dompurify
    
    added fix for dompurify isotope module SSR page where hydration and next canvas issue error hasbeen propely fixed
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * flex adjusted
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * Added packages
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    ---------
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

---
+++ app/blogs/[slug]/page.tsx
@@ -0,0 +1,101 @@
+import { error } from "console";
+import DOMPurify from "isomorphic-dompurify";
+import axios from "axios";
+// import "@/assets/css/BlogDetail.scss";
+import "@/public/assets/css/BlogDetail.scss";
+const apk = process.env.NEXT_PUBLIC_API_KEY;
+const apl = process.env.NEXT_PUBLIC_API_PARAMS;
+
+const headers = {
+  Authorization: `${apl}  ${apk}`,
+};
+
+// await fetch("http://example.com/api/endpoint", {
+//   method: "GET", // or "POST" or other HTTP methods
+//   headers: headers,
+// });
+const getData = async (slug: string): Promise<any> => {
+  const api = `http://127.0.0.1:8000/api/blogs/${slug}`;
+  const res = await axios.get(api, {
+    headers: {
+      Authorization: `${apl} ${apk}`,
+    },
+  });
+  return res.data;
+};
+
+interface Params {
+  slug: string;
+}
+const truncateContent = (content: string) => {
+  // Display between 40 and 50 characters
+  if (content.length <= 100000) {
+    return content;
+  }
+  return content.slice(0, 9000) + "...";
+};
+
+const SinglePage = async ({
+  params,
+}: {
+  params: Params;
+}): Promise<JSX.Element> => {
+  const { slug } = params;
+  const post = await getData(slug);
+  const clean = DOMPurify.sanitize(post.content);
+  return (
+    <>
+      <div className="blog-detail-container">
+        <div className="left-column">
+          <div className="header">
+            {/* <Image src={} alt="Blog Header" className="blog-header-image" /> */}
+          </div>
+          <div className="blog-details">
+            <h1 className="blog-title">{post.title}</h1>
+            {/* <p className="blog-description">{clean}</p> */}
+            <div
+              className="blog-description"
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(truncateContent(post.content)),
+              }}></div>
+            <div className="comment-box">
+              <h2>Comments</h2>
+              <div className="comment-list">
+                {/* {comments.map((comment, index) => (
+                  <div key={index} className="comment">
+                    <p className="comment-text">{comment}</p>
+                    <p className="comment-author">Author: John Doe</p>
+                  </div>
+                ))} */}
+              </div>
+              <div className="comment-form">
+                <h3>Add a Comment</h3>
+                <textarea
+                  rows={4}
+                  placeholder="Write your comment here..."
+                  // value=
+                ></textarea>
+                <button>Submit Comment</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="right-column">
+          <div className="post-list">
+            <h2>Post List</h2>
+            <div className="most-popular-posts">
+              <h3>Most Popular Posts</h3>
+              {/* Add popular posts here */}
+            </div>
+            <div className="topics">
+              <h3>Topics</h3>
+              {/* Add topics here */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SinglePage;
--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,46 +1,57 @@
 "use client";
 import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
-// import "../assets/css/blogpage.scss";
 import "@/public/assets/css/blogpage.scss";
+import Link from "next/link";
+import DOMPurify from "dompurify";
 
 export interface BlogPost {
   id: number;
   title: string;
   content: string;
+  slug: string;
 }
 
 export default function Blog() {
   const [blogPosts, setBlogPosts] = useState<BlogPost[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  //const apiURl = process.env.NEXT_PUBLIC_API_URL;
-  //console.log(apiURl);
-  const apiURl = "http://127.0.0.1:8000/api/blogs/";
+  const api = "http://127.0.0.1:8000/api/blogs/";
   const apk = process.env.NEXT_PUBLIC_API_KEY;
-
   const apl = process.env.NEXT_PUBLIC_API_PARAMS;
 
   const fetchBlogPosts = useCallback(async () => {
     setIsLoading(true);
     try {
-      const response = await axios.get(apiURl, {
+      const response = await axios.get(api, {
         headers: {
           Authorization: `${apl} ${apk}`,
         },
       });
       const newPosts = response.data;
-      setBlogPosts((prevPosts) => [...prevPosts, ...newPosts]);
+
+      setBlogPosts((prevPosts) => {
+        // Filter out posts that already exist in prevPosts
+        const uniqueNewPosts = newPosts.filter((newPost: BlogPost) =>
+          prevPosts.every((prevPost: BlogPost) => prevPost.id !== newPost.id)
+        );
+
+        return [...prevPosts, ...uniqueNewPosts];
+      });
     } catch (error) {
       console.error("Error fetching data:", error);
       // Display an error message to the user
     } finally {
       setIsLoading(false);
     }
-  }, [apiURl, apk, apl]);
+  }, [api, apk, apl]);
 
   const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
     const { scrollTop, clientHeight, scrollHeight } = e.currentTarget;
-    if (scrollHeight - scrollTop === clientHeight && !isLoading) {
+    if (
+      blogPosts.length > 10 &&
+      scrollHeight - scrollTop === clientHeight &&
+      !isLoading
+    ) {
       fetchBlogPosts();
     }
   };
@@ -49,13 +60,29 @@ export default function Blog() {
     fetchBlogPosts();
   }, [fetchBlogPosts]);
 
+  const truncateContent = (content: string) => {
+    // Display between 40 and 50 characters
+    if (content.length <= 100000) {
+      return content;
+    }
+    return content.slice(0, 90) + "...";
+  };
+
   return (
     <div className="blog-page">
-      <div className="blog-left" onScroll={handleScroll}>
+      <div className="blog-left">
         {blogPosts.map((post) => (
           <div className="blog-card" key={post.id}>
             <h2>{post.title}</h2>
-            <p>{post.content}</p>
+            {/* Sanitize and render the content */}
+            <div
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(truncateContent(post.content)),
+              }}></div>
+            {/* Use Link to navigate to the individual blog post page */}
+            <Link href={`/blogs/${encodeURIComponent(post.slug)}`}>
+              Read More
+            </Link>
           </div>
         ))}
         {isLoading && <p>Loading...</p>}
@@ -73,10 +100,7 @@ export default function Blog() {
         </div>
         <div className="popular-topics">
           <h3>Popular Topics</h3>
-          <ul>
-            {/* Render up to 4 popular topics here */}
-            {/* ... */}
-          </ul>
+          <ul>{/* Render up to 4 popular topics here */}</ul>
         </div>
         <div className="search">
           <input type="text" placeholder="Search..." />
