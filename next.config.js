fe1ad8a2bbaa60351415741839a71c4e041b9ef5
RajrupDasid <rajrupofficialid@protonmail.com>
2023-10-11 19:56:51 +0530

    Image fetching api domains added
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig}*/
 const nextConfig = {
+    trailingSlash: true,
+    images: {
+        domains: ['127.0.0.1','webstackapi.webstackpros.net']
+      },
 reactStrictMode: true,
 webpack: (config) => {
 config.externals = [...config.externals, "canvas", "jsdom"];
 return config;
 }
+
 }
 
 module.exports = nextConfig
