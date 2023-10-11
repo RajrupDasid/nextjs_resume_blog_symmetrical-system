c3022c30af3b7fd2ef0a614787cb0468163c2696
RajrupDasid <rajrupofficialid@protonmail.com>
2023-10-12 00:25:11 +0530

    Proper image fetching uri component added
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig}*/
 const nextConfig = {
-    trailingSlash: true,
-    images: {
-        domains: ['127.0.0.1','webstackapi.webstackpros.net']
-      },
+  trailingSlash: true,
+  images: {
+      domains: ['127.0.0.1',"s3.us-west-2.amazonaws.com"]
+    },
 reactStrictMode: true,
 webpack: (config) => {
 config.externals = [...config.externals, "canvas", "jsdom"];
