8cad7d567083aca6147747678356b53fb6cd626b
Rajrup Das <rajrupofficialid@protonmail.com>
2023-10-13 16:03:17 +0000

    Microsoft analytics added (#18)
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,12 @@ import { Inter } from "next/font/google";
 import NavBar from "./navbar/page";
 import Footer from "./footer/page";
 import GoogleAnalytics from "./GoogleAnalytics";
+import ClarityMS from "./Clarity";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://www.webstackpros.net"),
   title: "Webstackpros.net",
   description:
     "We are a team of professionals here to turn your digital dreams into reality. You imagine it, and we'll develop it. Thank you for visiting our site.",
@@ -21,11 +23,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NavBar />
+        {children}
+        <Footer />
         {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
           <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
         ) : null}
-        {children}
-        <Footer />
+        <ClarityMS />
       </body>
     </html>
   );
