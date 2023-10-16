b3de5de0c0610be6ab1c72f2d946307d745dca2e
RajrupDasid <rajrupofficialid@protonmail.com>
2023-10-16 08:16:35 +0530

    Rewritten pages for better description
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -8,9 +8,16 @@ export const metadata: Metadata = {
 };
 const services = [
   {
-    title: "Front-end Development",
+    title: "Security System",
     description:
-      "We specialize in building interactive and user-friendly interfaces.",
+      "We have experience developing robust security system that can prevent cloud to application exploit",
+    imageUrl:
+      "https://www.crio.do/blog/content/images/2021/04/Full-stack-development-blueprint.png", // Add the path to your image
+  },
+  {
+    title: "Ecommerce",
+    description:
+      "We specialize in building interactive and customer engaging Ecommerce sites for clients",
     imageUrl:
       "https://www.crio.do/blog/content/images/2021/04/Full-stack-development-blueprint.png", // Add the path to your image
   },
@@ -22,23 +29,16 @@ const services = [
       "https://www.crio.do/blog/content/images/2021/04/Full-stack-development-blueprint.png", // Add the path to your image
   },
   {
-    title: "Front-end Development",
+    title: "Devops",
     description:
-      "We specialize in building interactive and user-friendly interfaces.",
+      "We specialize in managing and developing complex cicd pipeline for complex projecs",
     imageUrl:
       "https://www.crio.do/blog/content/images/2021/04/Full-stack-development-blueprint.png", // Add the path to your image
   },
   {
-    title: "Front-end Development",
+    title: "Machine Learning and Analytics",
     description:
-      "We specialize in building interactive and user-friendly interfaces.",
-    imageUrl:
-      "https://www.crio.do/blog/content/images/2021/04/Full-stack-development-blueprint.png", // Add the path to your image
-  },
-  {
-    title: "Front-end Development",
-    description:
-      "We specialize in building interactive and user-friendly interfaces.",
+      "We specialize in building large machine learning models that can be used with any applications",
     imageUrl:
       "https://www.crio.do/blog/content/images/2021/04/Full-stack-development-blueprint.png", // Add the path to your image
   },
