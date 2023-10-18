a0a1f238c01c9846d68e597f3a7c81aa331b0cbf
Rajrup Das <rajrupofficialid@protonmail.com>
2023-10-18 16:50:07 +0000

    Site map updated with new urls (#24)
    * Microsoft analytics added
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * SEO optimized and some design improvements done
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * SEO optimized
    
    Design fixed for desktop
    
    Favicon changed
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * More responsiveness added
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * projects page changed
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * Rewritten pages for better description
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * Some pages fixed
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * Bunch of improvements added including contact from activated
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * Somethings hasbeen removed
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * Lots of changes added
    
    design improvements added
    
    responsove ness added
    
    breadcrumbs added
    
    more changes to come
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * Truncated content logic improved
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * Updated sitemaps
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    ---------
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    Signed-off-by: Rajrup Das <rajrupofficialid@protonmail.com>

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -42,6 +42,8 @@ export default async function sitemap() {
     {url:`${baseUrl}/services`,lastModified:new Date()},
     {url:`${baseUrl}/projects`,lastModified:new Date()},
     {url:`${baseUrl}/contact`,lastModified:new Date()},
+    {url:`${baseUrl}/blogs`,lastModified:new Date()},
+    {url:`${baseUrl}/privacy-policy`,lastModified:new Date()},
     ...postUrls,
   ];
 }
