f9bd723371b7008faabecd667b4939efe2921694
RajrupDasid <rajrupofficialid@protonmail.com>
2023-11-11 00:03:53 +0530

    cached data removed
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- next.config.js
+++ next.config.js
@@ -9,19 +9,6 @@ webpack: (config) => {
 config.externals = [...config.externals, "canvas", "jsdom"];
 return config;
 },
-async headers() {
-  return [
-    {
-      source: '/(.*)', // Match all routes
-      headers: [
-        {
-          key: 'Cache-Control',
-          value: 'public, max-age=31536000, immutable', // Cache for 1 year
-        },
-      ],
-    },
-  ];
-},
 }
 
 module.exports = nextConfig
